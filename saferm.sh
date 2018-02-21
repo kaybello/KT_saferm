17c6b306979ac26ba9d4e2a310822cb9096652c8
Kayode Bello <kayode.bello@MacBook-Pro.domain.name>
2018-02-21 12:03:01 +0100

    replacing the code in saferm.sh with what was in test.sh

--- saferm.sh
+++ saferm.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-# This is a bash script
-home="$HOME"
 trashSafermName=".Trash_saferm"
-trashSafermPath="$home/$trashSafermName"
+trashSafermPath="$HOME/$trashSafermName"
 FilePath=$1
-totalItemListing=$(ls -l "$FilePath" | sort -k1,1 | awk -F " " '{print $NF}' | sed -e '$ d' ) 
-item=i
+totalItemListing=$(ls -l "$FilePath")
 
 # create .trash_saferm if it doesn't exist
 
@@ -13,73 +10,56 @@ item=i
 #1 if it doesn't CREATE it
 if [ ! -d "$trashSafermPath" ];
 then
-     mkdir "$trashSafermPath"
-     echo "$trashSafermName created"
+    mkdir "$trashSafermPath"
+    echo "$trashSafermName created"
 fi
 
 
+if [[ -f $1 ]]
+then
+    echo "$1 is a file"
+    read -p "do you want to remove $1?" reply
+    #if the first letter of the reply is lower or upper case Y
+    if [[ $reply =~ ^[y*/Y*]$  ]];
+    then
+        mv $1 $HOME/.Trash_saferm
+        echo "$1 removed"
+    #else if the first letter of the reply is lower or upper case N
+    elif [[ $reply =~ ^[n*/N*]$ ]];
+    then
+        echo "can't be removed"
+    else
+        echo "error"
+    fi
 
-# check if the argument is a file or directory
-if [[ -f "$1" ]];
-then
-        echo "$1 is a file"
-elif [[ -d "$1" ]];
-then
-        echo "$1 is a directory"
 else
-        echo "$1 is not a file or directory"
-fi
+    #if its a directory
+    echo "$1 is a directory"
+    read -p "do you want to examine $1? " reply
 
-#for file
-    read -p "do you want to remove $i?"
-        if [[ $REPLY =~ ^[y*/Y*]$  ]];
-        then
-            mv $1 $HOME/.Trash_saferm
-            echo "$1 removed"
-        elif [[ $REPLY =~ ^[n*/N*]$ ]];
-        then
-            echo "can't be removed"
-        else
-            echo "error"
-
-        fi
-
-
-
-
-
-
-#for directory
-
-# to examine what is in the directory
-if [[ -d "$1" ]]
-then
-	#ask to examine the directory
-	read -p "do you want $1 to be examined"
-	if [[ $REPLY =~ ^[y*/Y*]$ ]]
-	then
-
-    		# examine each file
-    		ls $1
-            for i in $( ls $1 ); do
-            # Ask the user to delete the file or directory
-            read -p "do you want to remove $i?"
+    #if the first letter of the reply is lower or upper case Y
+    if [[ $reply =~ ^[y*/Y*]$ ]]
+    then
+        #examine each file
+        #ls $1
+        for i in $( ls $1 ); do
+           #Ask the user to delete the file or directory
+            read -p "do you want to remove $1? "
+            #if the first letter of the reply is lower or upper case Y
             if [[ $REPLY =~ ^[y*/Y*]$  ]];
             then
-            mv $1 $HOME/.Trash_saferm
-            echo "$1 removed"
+                mv $1 $HOME/.Trash_saferm
+                echo "$1 removed"
+            #if the first letter of the reply is lower or upper case N
             elif [[ $REPLY =~ ^[n*/N*]$ ]];
             then
-            echo "can't be removed"
+                echo "can't be removed"
             else
-            echo "error"
+                echo "error"
             fi
-            done
-    else
-            echo "why don't you want me to examine $1"
+        done
     fi
-    #loop through each directory
 
 fi
 
-
+#ask to delete file
